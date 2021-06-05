f2319075e7c7dd50b1244b89020d2c58b817efc5
Kari Johnson <kari@crownmediamarketing.com>
2021-06-05 17:00:57 -0600

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.css'
+
+function Footer() {
+    return (
+        <footer className="sticky footer mt-auto py-3" style={{ textAlign: "center" }}>
+            <div>
+                <span>&copy; 2020 Kari Johnson</span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
