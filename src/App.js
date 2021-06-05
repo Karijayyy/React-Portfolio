115bb3bef94a6e8d1b6ffa7ebc4845e37a404956
Kari Johnson <kari@crownmediamarketing.com>
2021-06-05 17:46:09 -0600

    api data for projects

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
-// Contact Form
-// import './App.css';
-// import ContactForm from './ContactForm';
+import Footer from "./components/Footer";
+
 
 //NavTabs component
 // import NavTabs from "./components/NavTabs";
@@ -24,15 +23,6 @@ function App() {
   );
 }
 
-// Need to figure out the syntax to get this working on this page to use the contact form 
-
-// const App = () => {
-//   return (
-//     <div className="App">                 
-//         <ContactForm />           
-//     </div>
-//   );
-// }
 
 
 export default App;
