11214647cefcfd0cece5670814b5f2f40de9d98d
Kari Johnson <kari@crownmediamarketing.com>
2021-06-09 19:49:25 -0600

    Updates

---
+++ precache-manifest.abd2da5bb8f92e404b9ba477aa854e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e503f7aef6a9aec2e543",
+    "url": "/static/css/main.180d2605.chunk.css"
+  },
+  {
+    "revision": "e503f7aef6a9aec2e543",
+    "url": "/static/js/main.9d70b5aa.chunk.js"
+  },
+  {
+    "revision": "b3952c23394853283555",
+    "url": "/static/js/runtime~main.dc611263.js"
+  },
+  {
+    "revision": "a7628594e5111afa4400",
+    "url": "/static/js/2.16a4895c.chunk.js"
+  },
+  {
+    "revision": "227be4ff931475e4bd59",
+    "url": "/static/js/3.a08d5cb2.chunk.js"
+  },
+  {
+    "revision": "de9c1975985cf88e23840dcea9b24fa6",
+    "url": "/index.html"
+  }
+];
